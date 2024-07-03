3ddc9d50115aafc4bbd4408b209781c385307606
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-03 18:14:47 +0600

    added baseApi

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,11 +1,22 @@
-import { selectTodos } from "@/redux/features/todo/todoSlice";
-import { useAppSelector } from "@/redux/hooks";
+import { useGetTodosQuery } from "@/redux/api/api";
+import { TTodo } from "@/redux/features/todo/todoSlice";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
-  const todos = useAppSelector(selectTodos);
+  //* From Local Redux Store
+  // const todos = useAppSelector(selectTodos);
+
+  //* From API
+
+  const { data, isLoading, isError } = useGetTodosQuery(undefined);
+  const todos = data?.data;
+  if (isLoading)
+    return <p className="text-center text-2xl font-bold">Loading...</p>;
+
+  if (isError) return <p className="text-center text-2xl font-bold">Error</p>;
+
   return (
     <div>
       <div className="mx-auto my-5 text-center space-x-16">
@@ -13,15 +24,13 @@ const TodoContainer = () => {
         <TodoFilter />
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl mx-auto p-10 space-y-5">
-        {todos.length === 0 && (
+        {todos.length !== 0 ? (
+          todos.map((todo: TTodo) => <TodoCard key={todo.id} todo={todo} />)
+        ) : (
           <div className="p-8 bg-white mx-auto rounded-md flex items-center justify-center gap-5">
             <p className="font-semibold text-2xl">There is no task </p>
           </div>
         )}
-
-        {todos.map((todo) => (
-          <TodoCard key={todo.id} todo={todo} />
-        ))}
       </div>
     </div>
   );
--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -2,6 +2,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "BACKEND API URL" }),
-  endpoints: () => ({}),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => ({
+        url: "/tasks",
+        method: "GET",
+      }),
+    }),
+  }),
 });
+
+export const { useGetTodosQuery } = baseApi;
